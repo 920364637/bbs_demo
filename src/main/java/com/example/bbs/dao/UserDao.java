cfbe31469e5ca4dbb69d986636119649bda44183
JacksonLaw577 <920364637@qq.com>
2022-08-25 23:25:31 +0800

    dao

--- src/main/java/com/example/bbs/dao/UserDao.java
+++ src/main/java/com/example/bbs/dao/UserDao.java
@@ -2,11 +2,12 @@ package com.example.bbs.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import com.example.bbs.mapper.UserMapper;
 import com.example.bbs.model.User;
 
-@Component
+@Repository
 public class UserDao {
 
     @Autowired
--- src/test/java/com/example/bbs/dao/UserDaoTests.java
+++ src/test/java/com/example/bbs/dao/UserDaoTests.java
@@ -17,7 +17,7 @@ public class UserDaoTests {
     void insert() {
 
         User user = new User();
-        user.setName("123561512");
+        user.setName("123561888512");
         user.setPassword("564565432");
 
         boolean flag = dao.insert(user);
@@ -31,7 +31,7 @@ public class UserDaoTests {
     @Test
     void update(){
         User user = dao.findUserByUserId(1956358874l);
-        user.setName("yesItsMRAWERAe");
+        user.setName("ohmygod");
         boolean flag = dao.update(user);
         if (!flag) {
             System.out.println("fail!");
