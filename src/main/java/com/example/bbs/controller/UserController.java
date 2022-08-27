77a4e09a755d22600b6c1f65d850cb414a741a81
JacksonLaw577 <920364637@qq.com>
2022-08-27 19:47:07 +0800

    user/register接口

--- src/main/java/com/example/bbs/controller/UserController.java
+++ src/main/java/com/example/bbs/controller/UserController.java
@@ -4,11 +4,13 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.bbs.response.user.LoginResponse;
+import com.example.bbs.response.user.RegisterResponse;
 import com.example.bbs.service.UserService;
 import com.example.bbs.util.Context;
 
@@ -18,7 +20,14 @@ public class UserController {
     @Autowired
     UserService service;
 
-    @GetMapping(value = "/login", produces = "application/json")
+    @PostMapping(value = "/register", produces = "application/json;charset=utf-8")
+    public Context.ResponseData register(@RequestParam Map<String, Object> params) {
+        Context context = new Context(params, new RegisterResponse());
+        service.register(context);
+        return context.getResponse();
+    }
+
+    @GetMapping(value = "/login", produces = "application/json;charset=utf-8")
     public Context.ResponseData login(@RequestParam Map<String, Object> params) {
         Context context = new Context(params, new LoginResponse());
         service.Login(context);
